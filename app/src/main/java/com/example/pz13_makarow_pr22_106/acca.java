365e2fbf517bce47402e9e5a300765fc6b99f3d5
egor <porem.578@gmail.com>
2023-06-18 20:55:17 +0700

    4

--- app/src/main/java/com/example/pz13_makarow_pr22_106/acca.java
+++ app/src/main/java/com/example/pz13_makarow_pr22_106/acca.java
@@ -8,12 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class acca extends AppCompatActivity implements View.OnClickListener {
     ImageView menu;
     ImageButton player;
     ImageButton profile;
     Intent in;
+    ImageView calm;
+    ImageView relax;
+    ImageView focus;
+    ImageView anxious;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,14 @@ public class acca extends AppCompatActivity implements View.OnClickListener {
         player.setOnClickListener(this);
         profile = (ImageButton) findViewById(R.id.imageButton3);
         profile.setOnClickListener(this);
+        calm = (ImageView) findViewById(R.id.imageView11);
+        calm.setOnClickListener(this);
+        relax = (ImageView) findViewById(R.id.imageView10);
+        relax.setOnClickListener(this);
+        focus = (ImageView) findViewById(R.id.imageView9);
+        focus.setOnClickListener(this);
+        anxious = (ImageView) findViewById(R.id.imageView8);
+        anxious.setOnClickListener(this);
     }
 
     @Override
@@ -40,6 +53,18 @@ public class acca extends AppCompatActivity implements View.OnClickListener {
             case R.id.imageButton3:
                 in = new Intent(this, Profile.class);
                 startActivity(in);
+                break;
+            case R.id.imageView11:
+                Toast.makeText(this,"спокойствие", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageView10:
+                Toast.makeText(this,"расслабленность", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageView9:
+                Toast.makeText(this,"сосредоточность", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageView8:
+                Toast.makeText(this,"волнение", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
